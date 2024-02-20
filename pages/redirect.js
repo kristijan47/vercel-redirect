bd99da29a54ae44b347e9dde5275addf07cf5f71
kristijan47 <60441523+kristijan47@users.noreply.github.com>
2024-02-20 12:32:33 +0100

    Update redirect.js

--- pages/redirect.js
+++ pages/redirect.js
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const targetUrl = 'https://livingamonganimals.com/introducing-augie-the-remarkable-20-year-old-golden-retriever-who-has-recently-made-history-as-the-oldest-dog-of-her-breed-ever-recorded-2/';
+  const targetUrl = 'https://livingamonganimals.com/';
 
   await delay(3000);
 
